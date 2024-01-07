1985a056aeee24d1a3d6bfbeb51f430e8e1df03d
cinta rahmalya syifa <112394845+CintaaaRasyifa@users.noreply.github.com>
2024-01-07 22:41:11 +0700

    script done- Add komen

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-const box = document.getElementById('box');
-const text = document.getElementById('text');
-let mousedown = false;
-let position_x;
-let position_y;
+const box = document.getElementById('box'); 
+const text = document.getElementById('text'); 
+let mousedown = false; // simpan status klik mouse
+let position_x; // simpan posisi horizontal
+let position_y; // simpan posisi vertikal
 
 function change() {
-    box.style.left = position_x  + "px";
-    box.style.top = position_y + "px";
-    text.style.left = -position_x + "px";
-    text.style.top = -position_y + "px";
+    box.style.left = position_x  + "px"; // ubah posisi horizontal elemen box
+    box.style.top = position_y + "px"; // ubah posisi vertikal elemen box
+    text.style.left = -position_x + "px"; // ubah posisi horizontal elemen text
+    text.style.top = -position_y + "px"; // ubah posisi vertikal elemen text
 }
 
 box.addEventListener("mousedown", function (e) {
-    mousedown = true;
+    mousedown = true; // Saat ditekan pada elemen box, ubah nilai mousedown menjadi true
 });
 
 document.addEventListener("mousemove", function (e) {
    if (mousedown) {
-        position_x = e.clientX - 200;
-        position_y = e.clientY - 200;
-        change();
-        console.log(position_x);
-        console.log(position_y);
+        // Jika mouse digerakkan saat masih ditekan, ubah posisi_x dan position_y berdasarkan posisi mouse relatif terhadap elemen box
+        position_x = e.clientX - 200; // Menghitung posisi horizontal berdasarkan posisi mouse
+        position_y = e.clientY - 200; // Menghitung posisi vertikal berdasarkan posisi mouse
+        change(); // Memanggil fungsi change untuk memperbarui tampilan elemen box dan text
+        console.log(position_x); // Mencetak posisi_x ke konsol
+        console.log(position_y); // Mencetak posisi_y ke konsol
    } 
 })
 
 document.addEventListener("mouseup", function (e) {
-    mousedown = false;
+    mousedown = false; // Saat mouse dilepas, ubah nilai mousedown menjadi false
 });
 
-change();
+change(); 
